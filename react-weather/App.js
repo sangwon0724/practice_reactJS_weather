9b584eb10cb6910b6c964b6dc66cb228f4040055
sangwon0724 <leews0724@naver.com>
2024-02-06 09:25:43 +0900

    패키지 업데이트

--- react-weather/App.js
+++ react-weather/App.js
@@ -18,6 +18,7 @@ export default class extends React.Component {
     } } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
+    console.log("타나?");
     this.setState({
       isLoading: false,
       condition: weather[0].main,
